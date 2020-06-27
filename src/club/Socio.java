edb8d64d22d5b38fc6633ec60eb6b2bfbc7d51cd
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-06-27 16:10:44 -0300

    Socio

---
+++ src/club/Socio.java
@@ -0,0 +1,32 @@
+package club;
+
+import java.time.LocalDate;
+
+public class Socio {
+
+	private int numero;
+	private String nombre;
+	private LocalDate fechaIngreso;
+	
+	public Socio(int numero, String nombre, LocalDate fechaIngreso) {
+		
+		this.numero = numero;
+		this.nombre = nombre;
+		this.fechaIngreso = fechaIngreso;
+	}
+	
+	public int obtenerNumero() {
+		
+		return this.numero;
+	}
+	
+	public String obtenerNombre() {
+		
+		return this.nombre;
+	}
+	
+	public LocalDate obtenerFechaIngreso() {
+		
+		return this.fechaIngreso;
+	}
+}
