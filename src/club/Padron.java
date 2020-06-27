f1a70588d3fe045570e98ed4668f3967c9f7b53a
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-06-27 16:13:23 -0300

    Padrón

---
+++ src/club/Padron.java
@@ -0,0 +1,13 @@
+package club;
+
+/**
+ * Padrón de Socios del Club.
+ *
+ */
+public class Padron {
+
+	public static final int PRIMER_NUMERO = 4001;
+	public static final int CANTIDAD_MAXIMA = 100;
+	public static final int ULTIMO_NUMERO = PRIMER_NUMERO + CANTIDAD_MAXIMA - 1;
+
+}
